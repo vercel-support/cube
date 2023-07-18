783dd16c10b568d0907b72cce53abf83e12194b5
Riz <rizwana.akmal@hotmail.com>
2023-07-18 17:49:05 +0100

    ignoreBuildErrors

--- docs/docs-new/next.config.mjs
+++ docs/docs-new/next.config.mjs
@@ -4,12 +4,19 @@ import remarkFrontmatter from "remark-frontmatter";
 import remarkHtmlKatex from "remark-html-katex";
 import linkEnvironmentVariables from "./plugins/link-environment-variables.mjs";
 
-import redirects from './redirects.json' assert { type: "json" };
+import redirects from "./redirects.json" assert { type: "json" };
 
 /**
  * @type {import('next').NextConfig}
  */
 const config = {
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
   basePath: process.env.BASE_PATH || "",
   async redirects() {
     return redirects;
